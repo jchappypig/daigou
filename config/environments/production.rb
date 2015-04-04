71ebf78e5bc33b056678099e3fd84643f9d8a64b
Huanhuan Huang <jchappypig@hotmail.com>
2015-04-04 13:36:03 +1100

    setup email server.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {host: 'audaigou.herokuapp.com'}
+  config.action_mailer.default_options = {from: 'jchappypig@hotmail.com'}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.live.com',
+      :port                 => 587,
+      :domain               => 'audaigou.herokuapp.com',
+      :user_name            => ENV['MAIL_USERNAME'],
+      :password             => ENV['MAIL_PASSWORD'],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
