9b671e1ea4b1e76b46238e3afe41886bcc911d44
Huanhuan Huang <jchappypig@hotmail.com>
2015-04-17 20:58:48 +1000

    set action  permission for products.

--- app/controllers/casein/products_controller.rb
+++ app/controllers/casein/products_controller.rb
@@ -4,7 +4,7 @@ module Casein
   class ProductsController < Casein::CaseinController
 
     ## optional filters for defining usage according to Casein::AdminUser access_levels
-    # before_filter :needs_admin, :except => [:action1, :action2]
+    before_filter :needs_admin, :except => [:index]
     # before_filter :needs_admin_or_current_user, :only => [:action1, :action2]
 
     def index
