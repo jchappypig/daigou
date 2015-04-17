7ec3825b514936b4c501f1c22639eda14eeabc82
Huanhuan Huang <jchappypig@hotmail.com>
2015-04-17 23:37:03 +1000

    added select2

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'rails_12factor', group: :production
 
 gem 'casein'
 gem 'spreadsheet'
+gem 'select2-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/casein/custom.js
+++ app/assets/javascripts/casein/custom.js
@@ -1,2 +1,9 @@
 // CASEIN CUSTOM
 // Use this file for your project-specific Casein JavaScript
+
+//= require select2
+//= require select2_locale_zh-CN
+
+$(document).ready(function() {
+    $("#order_casein_admin_user_id").select2();
+});
