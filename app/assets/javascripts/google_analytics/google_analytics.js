7320b2a32734890c5d4dc68f524719ee1560bdf7
Huanhuan Huang <huanhuan.huang@betterbills.com>
2015-05-08 09:54:24 +1000

    Revert "make google analytics works with turbol link"
    This reverts commit d1e930bed5d316e42586aaec1c69907461bd35d5.

--- app/assets/javascripts/google_analytics/google_analytics.js
+++ app/assets/javascripts/google_analytics/google_analytics.js
@@ -1,50 +1,7 @@
-this.GoogleAnalytics = (function() {
-    function GoogleAnalytics() {}
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    GoogleAnalytics.load = function() {
-        var firstScript, ga;
-        window._gaq = [];
-        window._gaq.push(["_setAccount", GoogleAnalytics.analyticsId()]);
-        ga = document.createElement("script");
-        ga.type = "text/javascript";
-        ga.async = true;
-        ga.src = ("https:" === document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
-        firstScript = document.getElementsByTagName("script")[0];
-        firstScript.parentNode.insertBefore(ga, firstScript);
-        if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-            return document.addEventListener("page:change", (function() {
-                return GoogleAnalytics.trackPageview();
-            }), true);
-        } else {
-            return GoogleAnalytics.trackPageview();
-        }
-    };
-
-    GoogleAnalytics.trackPageview = function(url) {
-        if (!GoogleAnalytics.isLocalRequest()) {
-            if (url) {
-                window._gaq.push(["_trackPageview", url]);
-            } else {
-                window._gaq.push(["_trackPageview"]);
-            }
-            return window._gaq.push(["_trackPageLoadTime"]);
-        }
-    };
-
-    GoogleAnalytics.isLocalRequest = function() {
-        return GoogleAnalytics.documentDomainIncludes("local");
-    };
-
-    GoogleAnalytics.documentDomainIncludes = function(str) {
-        return document.domain.indexOf(str) !== -1;
-    };
-
-    GoogleAnalytics.analyticsId = function() {
-        return 'UA-62284924-1';
-    };
-
-    return GoogleAnalytics;
-
-})();
-
-GoogleAnalytics.load();
+ga('create', 'UA-62284924-1', 'auto');
+ga('send', 'pageview');
